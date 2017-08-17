85368ad506b27cacb26dee4bbff544d247285d06
zjun <565061763@qq.com>
2017-08-17 18:17:55 +0800

    demo测试

--- app/src/main/java/com/fanwe/www/runnable/MainActivity.java
+++ app/src/main/java/com/fanwe/www/runnable/MainActivity.java
@@ -1,15 +1,23 @@
 package com.fanwe.www.runnable;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.fanwe.library.windowmanager.SDDraggableFloatView;
 
 public class MainActivity extends AppCompatActivity
 {
+
+    private SDDraggableFloatView mFloatView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFloatView = new SDDraggableFloatView(this);
+        mFloatView.setContentView(findViewById(R.id.btn));
+        mFloatView.attachToWindow(true);
     }
 }

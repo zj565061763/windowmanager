e49995d37e649b0ed52b528389237b7b4a2b195e
zjun <565061763@qq.com>
2017-08-18 12:26:12 +0800

    demo测试

--- app/src/main/java/com/fanwe/www/windowmanager/MainActivity.java
+++ app/src/main/java/com/fanwe/www/windowmanager/MainActivity.java
@@ -4,17 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Toast;
 
 import com.fanwe.library.windowmanager.SDFloatHelper;
-
-import ezy.assist.compat.SettingsCompat;
 
 public class MainActivity extends AppCompatActivity
 {
 
     private SDFloatHelper mFloatHelper = new SDFloatHelper();
-    private SDFloatHelper mOnStopFloatHelper = new SDFloatHelper();
+    private SDFloatHelper mNewActivityFloatHelper = new SDFloatHelper();
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -22,21 +19,13 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mFloatHelper.setContentView(findViewById(R.id.btn)); //设置要悬浮的view
-
-        mOnStopFloatHelper.setContentView(findViewById(R.id.btn_onstop)); //设置要悬浮的view
+        mFloatHelper.setContentView(findViewById(R.id.btn_float)); //设置要悬浮的view
+        mNewActivityFloatHelper.setContentView(findViewById(R.id.btn_new_activity_float)); //设置要悬浮的view
     }
 
     public void onClickAddToWindow(View view)
     {
-        if (SettingsCompat.canDrawOverlays(this))
-        {
-            mFloatHelper.addToWindow(true); //true-添加到Window
-        } else
-        {
-            SettingsCompat.manageDrawOverlays(this);
-            Toast.makeText(this, "请打开悬浮窗权限", Toast.LENGTH_SHORT).show();
-        }
+        mFloatHelper.addToWindow(true); //true-添加到Window
     }
 
     public void onClickRemoveFromWindow(View view)
@@ -58,14 +47,14 @@ public class MainActivity extends AppCompatActivity
     protected void onResume()
     {
         super.onResume();
-        mOnStopFloatHelper.restoreContentView();
+        mNewActivityFloatHelper.restoreContentView();
     }
 
     @Override
     protected void onStop()
     {
         super.onStop();
-        mOnStopFloatHelper.addToWindow(true);
+        mNewActivityFloatHelper.addToWindow(true);
     }
 
     @Override
@@ -73,9 +62,6 @@ public class MainActivity extends AppCompatActivity
     {
         super.onDestroy();
         mFloatHelper.addToWindow(false);
-        mFloatHelper.setContentView(null);
-
-        mOnStopFloatHelper.addToWindow(false);
-        mOnStopFloatHelper.setContentView(null);
+        mNewActivityFloatHelper.addToWindow(false);
     }
 }

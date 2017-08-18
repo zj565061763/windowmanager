bb3d93565ab9e6a7be8527c42799978135700529
zjun <565061763@qq.com>
2017-08-18 16:18:43 +0800

    重构

--- lib/src/main/java/com/fanwe/library/windowmanager/SDDraggableFloatView.java
+++ lib/src/main/java/com/fanwe/library/windowmanager/SDDraggableFloatView.java
@@ -5,9 +5,8 @@ import android.view.MotionEvent;
 import android.view.View;
 
 /**
- * Created by Administrator on 2017/8/18.
+ * 可拖动的悬浮view
  */
-
 public class SDDraggableFloatView extends SDFloatView
 {
     public SDDraggableFloatView(Context context)
@@ -31,97 +30,125 @@ public class SDDraggableFloatView extends SDFloatView
     }
 
     @Override
-    public boolean onInterceptTouchEvent(MotionEvent ev)
+    public void setContentView(View view)
     {
-        return super.onInterceptTouchEvent(ev) || mIsDraggable;
+        View oldView = getContentView();
+        if (oldView != null)
+        {
+            oldView.setOnTouchListener(null);
+        }
+
+        super.setContentView(view);
+
+        if (view != null)
+        {
+            view.setOnTouchListener(mInternalOnTouchListener);
+        }
     }
+
+    private OnTouchListener mInternalOnTouchListener = new OnTouchListener()
+    {
+        @Override
+        public boolean onTouch(View v, MotionEvent event)
+        {
+            processTouchEvent(event);
+            return false;
+        }
+    };
 
     private int mLastX;
     private int mLastY;
 
-    @Override
-    public boolean onTouchEvent(MotionEvent event)
+    private void processTouchEvent(MotionEvent event)
     {
-        final View view = getContentView();
-        if (mIsDraggable && view != null)
+        if (!mIsDraggable)
         {
-            switch (event.getAction())
-            {
-                case MotionEvent.ACTION_DOWN:
-                    mLastX = (int) event.getRawX();
-                    mLastY = (int) event.getRawY();
-                    break;
-                case MotionEvent.ACTION_MOVE:
-                    final int screenWidth = getContext().getResources().getDisplayMetrics().widthPixels;
-                    final int screenHeight = getContext().getResources().getDisplayMetrics().heightPixels;
-                    final int maxX = screenWidth - view.getWidth();
-                    final int maxY = screenHeight - view.getHeight();
-
-                    final int moveX = (int) event.getRawX();
-                    final int moveY = (int) event.getRawY();
-
-                    int dx = moveX - mLastX;
-                    int dy = moveY - mLastY;
-
-                    mLastX = moveX;
-                    mLastY = moveY;
-
-                    if (dx > 0)
-                    {
-                        if (view.canScrollHorizontally(-1))
-                        {
-                            dx = 0;
-                        }
-                    } else if (dx < 0)
-                    {
-                        if (view.canScrollHorizontally(1))
-                        {
-                            dx = 0;
-                        }
-                    }
-
-                    if (dy > 0)
-                    {
-                        if (view.canScrollVertically(-1))
-                        {
-                            dy = 0;
-                        }
-                    } else if (dy < 0)
-                    {
-                        if (view.canScrollVertically(1))
-                        {
-                            dy = 0;
-                        }
-                    }
-
-                    int x = getWindowParams().x + dx;
-                    int y = getWindowParams().y + dy;
-
-                    if (x < 0)
-                    {
-                        x = 0;
-                    } else if (x > maxX)
-                    {
-                        x = maxX;
-                    }
-
-                    if (y < 0)
-                    {
-                        y = 0;
-                    } else if (y > maxY)
-                    {
-                        y = maxY;
-                    }
-
-                    if (getWindowParams().x != x || getWindowParams().y != y)
-                    {
-                        getWindowParams().x = x;
-                        getWindowParams().y = y;
-                        SDWindowManager.getInstance().updateViewLayout(this, getWindowParams());
-                    }
-                    break;
-            }
+            return;
         }
-        return super.onTouchEvent(event);
+        final View view = getContentView();
+        if (view == null)
+        {
+            return;
+        }
+        if (!isAddedToWindow())
+        {
+            return;
+        }
+        switch (event.getAction())
+        {
+            case MotionEvent.ACTION_DOWN:
+                mLastX = (int) event.getRawX();
+                mLastY = (int) event.getRawY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                final int screenWidth = getContext().getResources().getDisplayMetrics().widthPixels;
+                final int screenHeight = getContext().getResources().getDisplayMetrics().heightPixels;
+                final int maxX = screenWidth - view.getWidth();
+                final int maxY = screenHeight - view.getHeight();
+
+                final int moveX = (int) event.getRawX();
+                final int moveY = (int) event.getRawY();
+
+                int dx = moveX - mLastX;
+                int dy = moveY - mLastY;
+
+                mLastX = moveX;
+                mLastY = moveY;
+
+                if (dx > 0)
+                {
+                    if (view.canScrollHorizontally(-1))
+                    {
+                        dx = 0;
+                    }
+                } else if (dx < 0)
+                {
+                    if (view.canScrollHorizontally(1))
+                    {
+                        dx = 0;
+                    }
+                }
+
+                if (dy > 0)
+                {
+                    if (view.canScrollVertically(-1))
+                    {
+                        dy = 0;
+                    }
+                } else if (dy < 0)
+                {
+                    if (view.canScrollVertically(1))
+                    {
+                        dy = 0;
+                    }
+                }
+
+                int x = getWindowParams().x + dx;
+                int y = getWindowParams().y + dy;
+
+                if (x < 0)
+                {
+                    x = 0;
+                } else if (x > maxX)
+                {
+                    x = maxX;
+                }
+
+                if (y < 0)
+                {
+                    y = 0;
+                } else if (y > maxY)
+                {
+                    y = maxY;
+                }
+
+                if (getWindowParams().x != x || getWindowParams().y != y)
+                {
+                    getWindowParams().x = x;
+                    getWindowParams().y = y;
+                    SDWindowManager.getInstance().updateViewLayout(this, getWindowParams());
+                }
+                break;
+        }
     }
 }
